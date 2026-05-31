2146225550e872b54f9967ba7af4a5f9273d780e
GKovtonyuk <112175748+GKovtonyuk@users.noreply.github.com>
2026-06-01 00:09:51 +0300

    Delete js directory

--- js/descriptions.js
+++
@@ -1,7 +0,0 @@
-const descriptions={
-Tetrahedron:{title:'Тетраедр',faces:4,vertices:4,edges:6,text:'Найпростіше платонове тіло.'},
-Cube:{title:'Куб',faces:6,vertices:8,edges:12,text:'Має квадратні грані.'},
-Octahedron:{title:'Октаедр',faces:8,vertices:6,edges:12,text:'Складається з трикутників.'},
-Dodecahedron:{title:'Додекаедр',faces:12,vertices:20,edges:30,text:'Має п’ятикутні грані.'},
-Icosahedron:{title:'Ікосаедр',faces:20,vertices:12,edges:30,text:'Має 20 трикутних граней.'}
-};
--- js/solids.js
+++
@@ -1,9 +0,0 @@
-function createGeometry(type){
-switch(type){
-case 'Tetrahedron': return new THREE.TetrahedronGeometry(1);
-case 'Cube': return new THREE.BoxGeometry(1,1,1);
-case 'Octahedron': return new THREE.OctahedronGeometry(1);
-case 'Dodecahedron': return new THREE.DodecahedronGeometry(1);
-case 'Icosahedron': return new THREE.IcosahedronGeometry(1);
-}
-}
